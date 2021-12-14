823600e81caff11671c55c6767d91f5e173fd785
unknown <tomi.molander@aalto.fi>
2021-12-14 16:44:33 +0200

    3.19-3.21

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
 
 const app = express()
 const Person = require('./models/person')
@@ -30,6 +29,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -78,7 +79,7 @@ app.get('/api/persons/:id', (request, response, next) => {
     })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
 
@@ -88,22 +89,17 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    Person.find({}).then(persons => {
-      if (persons.map(person => person.name).includes(body.name)) {
-        return response.status(400).json({ 
-          error: 'name must be unique' 
-        })
-      }else{
-        const person = new Person({
-          name: body.name,
-          number: body.number,
-        })
-  
-        person.save().then(savedPerson => {
-          response.json(savedPerson)
-        })
-      }
+    const person = new Person({
+      name: body.name,
+      number: body.number,
     })
+
+    person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+
   })
 
 app.delete('/api/persons/:id', (request, response, next) => {
